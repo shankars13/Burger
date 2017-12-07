554e1caf2b040bf9fc29bf757b071627bdb77ee9
shubha shankar <shankar.shub@gmail.com>
2017-12-07 11:53:18 -0800

    add heroku conn

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,41 @@
 // Set up MySQL Connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port:3306,
-  host: "localhost",
-  user: "root",
-  password: "Mysqpass!1",
-  database: "burgers_db"
+
+//*********** LOCALHOST Connection ************/
+// var connection = mysql.createConnection({
+//   port:3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "Mysqpass!1",
+//   database: "burgers_db"
+// });
+
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+//***********************************************/
+
+**************   HEROKU connection ************** 
+var connection = mysql.createPool({
+    host: 'us-cdbr-iron-east-05.cleardb.net',
+    user: 'ba39536af8aeb6',
+    password: '8a72570e',
+    database: 'heroku_ec767bef39bff76',
+    connectionLimit: 5
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+connection.getConnection(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.error("Connected to : " + connection.threadId);
 });
 
 module.exports = connection;
